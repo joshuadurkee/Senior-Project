bd9d50359166d3b53d083dcb574990cea60b5998
Joshua Durkee <joshua.durkee@oit.edu>
2012-09-11 11:45:53 -0700

    More cubes
    Cubes are now created when the X+ direction of the cube it activated.

--- Senior Project/Assets/Scripts/StartCubeGrow.cs
+++ Senior Project/Assets/Scripts/StartCubeGrow.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 using System.Collections;
 
 public class StartCubeGrow : MonoBehaviour {
-
+	
+	public GameObject cube;
+	
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
@@ -17,34 +19,31 @@ public class StartCubeGrow : MonoBehaviour {
 			Ray ray = camera.ScreenPointToRay(new Vector3(camera.pixelWidth/2, camera.pixelHeight/2, 0));
 			if (Physics.Raycast(ray, out hit, 10))
 			{
+				
 				if (hit.collider.gameObject.tag == "CubeSideX+")
 				{
-					//hit.collider.gameObject.GetComponent<CubeGrow>().scale = 1;
-					hit.collider.gameObject.transform.parent.animation.Play("GrowX+");
+					//hit.collider.gameObject.transform.parent.animation.Play("GrowX+");
+					GameObject newCube = Instantiate(cube, this.transform.position, Quaternion.identity) as GameObject;
+					newCube.transform.position += new Vector3(5,0,0);
 				}
 				if (hit.collider.gameObject.tag == "CubeSideX-")
 				{
-					//hit.collider.gameObject.GetComponent<CubeGrow>().scale = 1;
 					hit.collider.gameObject.transform.parent.animation.Play("GrowX-");
 				}
 				if (hit.collider.gameObject.tag == "CubeSideY+")
 				{
-					//hit.collider.gameObject.GetComponent<CubeGrow>().scale = 1;
 					hit.collider.gameObject.transform.parent.animation.Play("GrowY+");
 				}
 				if (hit.collider.gameObject.tag == "CubeSideY-")
 				{
-					//hit.collider.gameObject.GetComponent<CubeGrow>().scale = 1;
 					hit.collider.gameObject.transform.parent.animation.Play("GrowY-");
 				}
 				if (hit.collider.gameObject.tag == "CubeSideZ+")
 				{
-					//hit.collider.gameObject.GetComponent<CubeGrow>().scale = 1;
 					hit.collider.gameObject.transform.parent.animation.Play("GrowZ+");
 				}
 				if (hit.collider.gameObject.tag == "CubeSideZ-")
 				{
-					//hit.collider.gameObject.GetComponent<CubeGrow>().scale = 1;
 					hit.collider.gameObject.transform.parent.animation.Play("GrowZ-");
 				}
 			}
